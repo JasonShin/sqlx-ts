f4cc4073e9fc167609cc8827a9dc72d194bd92f7
Jason Shin <visualbbasic@gmail.com>
2022-04-03 22:37:48 +1000

    test: adding a failure test case in integration tests

---
+++ tests/postgres_test_failure-path1.rs
@@ -0,0 +1,20 @@
+use assert_cmd::prelude::*;
+use predicates::prelude::*;
+use std::process::Command;
+
+/// should be using all CLI args to provide credential for DB connection
+#[test]
+fn failure_with_all_cli_args() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("sqlx-ts").unwrap();
+
+    cmd.arg("samples/postgres/failure-path1")
+        .arg("--db-host=localhost")
+        .arg("--db-port=54321")
+        .arg("--db-user=postgres")
+        .arg("--db-pass=postgres");
+    cmd.assert()
+        .failure()
+        .stdout(predicates::str::contains("SQLs failed to compile!"));
+
+    Ok(())
+}
