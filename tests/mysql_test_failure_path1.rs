2fc4331aa431227b9b00b9b525861a8df44dc8be
Jason Shin <visualbbasic@gmail.com>
2022-04-17 21:45:23 +1000

    test: adding failure path test for mysql

--- tests/mysql_test_failure_path1.rs
+++ tests/mysql_test_failure_path1.rs
@@ -1,1 +1,24 @@
+#[cfg(test)]
+mod mysql_failure_path_tests {
+    use assert_cmd::prelude::*;
+    use predicates::prelude::*;
+    use std::process::Command;
 
+    /// should be using all CLI args to provide credential for DB connection
+    #[test]
+    fn failure_with_all_cli_args() -> Result<(), Box<dyn std::error::Error>> {
+        let mut cmd = Command::cargo_bin("sqlx-ts").unwrap();
+
+        cmd.arg("samples/generic/failure-path1")
+            .arg("--db-type=mysql")
+            .arg("--db-host=127.0.0.1")
+            .arg("--db-port=33306")
+            .arg("--db-user=root")
+            .arg("--db-name=sqlx-ts");
+        cmd.assert()
+            .failure()
+            .stdout(predicates::str::contains("SQLs failed to compile!"));
+
+        Ok(())
+    }
+}
--- tests/postgres_test_failure-path1.rs
+++ tests/postgres_test_failure-path1.rs
@@ -1,20 +1,23 @@
-use assert_cmd::prelude::*;
-use predicates::prelude::*;
-use std::process::Command;
+#[cfg(test)]
+mod postgres_failure_path_tests {
+    use assert_cmd::prelude::*;
+    use predicates::prelude::*;
+    use std::process::Command;
 
-/// should be using all CLI args to provide credential for DB connection
-#[test]
-fn failure_with_all_cli_args() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cmd = Command::cargo_bin("sqlx-ts").unwrap();
+    /// should be using all CLI args to provide credential for DB connection
+    #[test]
+    fn failure_with_all_cli_args() -> Result<(), Box<dyn std::error::Error>> {
+        let mut cmd = Command::cargo_bin("sqlx-ts").unwrap();
 
-    cmd.arg("samples/generic/failure-path1")
-        .arg("--db-host=localhost")
-        .arg("--db-port=54321")
-        .arg("--db-user=postgres")
-        .arg("--db-pass=postgres");
-    cmd.assert()
-        .failure()
-        .stdout(predicates::str::contains("SQLs failed to compile!"));
+        cmd.arg("samples/generic/failure-path1")
+            .arg("--db-host=localhost")
+            .arg("--db-port=54321")
+            .arg("--db-user=postgres")
+            .arg("--db-pass=postgres");
+        cmd.assert()
+            .failure()
+            .stdout(predicates::str::contains("SQLs failed to compile!"));
 
-    Ok(())
+        Ok(())
+    }
 }
