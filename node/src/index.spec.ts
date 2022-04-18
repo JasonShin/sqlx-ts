b58e7bad36c593d34f8fe1d2b3a2b5237a099bfc
Jason Shin <visualbbasic@gmail.com>
2022-04-18 21:34:03 +1000

    chore: add lint

--- node/src/index.spec.ts
+++ node/src/index.spec.ts
@@ -1,5 +1,27 @@
 import { sql } from './index'
 
-test('sql tagged template literal should return raw sql back', () => {
+test('should return single line', () => {
     const rawSql = sql`SELECT * FROM test;`
+
+    expect(rawSql).toBe('SELECT * FROM test;')
+})
+
+test('should return multiple lines', () => {
+    const rawSql = sql`
+        SELECT *
+        FROM test
+        WHERE createdAt > 2019-1-1;
+    `
+
+    expect(rawSql).toBe(`
+        SELECT *
+        FROM test
+        WHERE createdAt > 2019-1-1;
+    `)
+})
+
+test('should return empty', () => {
+    const rawSql = sql``
+
+    expect(rawSql).toBe('')
 })
--- node/src/index.ts
+++ node/src/index.ts
@@ -1,3 +1,7 @@
-export const sql = (query: any) => {
-    return query
+export const sql = (query: TemplateStringsArray) => {
+    if (query[0] === undefined || query[0] === null) {
+        return null
+    }
+
+    return query[0]
 }
