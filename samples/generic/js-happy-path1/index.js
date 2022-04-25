3d4a1aeb123bfe21588ab813fe414b4f5b7deaed
Jason Shin <visualbbasic@gmail.com>
2022-04-25 11:18:51 +1000

    chore: fixing js samples

--- samples/generic/js-happy-path1/index.js
+++ samples/generic/js-happy-path1/index.js
@@ -1,10 +1,10 @@
 import { sql } from 'sqlx-ts'
 
 // Querying from an unknown table
-const someQuery = sql`SELECT * FROM itemzz`;
+const someQuery = sql`SELECT * FROM items`;
 
 // Inserting more values than expected
 const insertQuery = sql`
     INSERT INTO items (food_type, time_takes_to_cook, table_id, points)
-    VALUES ('steak', 1, 1, 1, 1);
+    VALUES ('steak', 1, 1, 1);
 `
--- samples/generic/js-happy-path1/lazy-loaded.js
+++ samples/generic/js-happy-path1/lazy-loaded.js
@@ -3,14 +3,14 @@
 function test() {
     const { sql: sqlx } = require('sqlx-ts')
 
-    const someQuery = sqlx`SELECT * FROM unknown`
+    const someQuery = sqlx`SELECT * FROM items`
 
     return someQuery
 }
 
 function test2() {
     const SQLX = require('sqlx-ts')
-    const someQuery = SQLX.sql`SELECT * FROM unknown`
+    const someQuery = SQLX.sql`SELECT * FROM items`
 
     return someQuery
 }
