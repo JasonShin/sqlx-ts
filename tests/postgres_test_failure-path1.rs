57b816bf099406b256ec9bf25d881a0f16949efe
Jason Shin <visualbbasic@gmail.com>
2022-04-25 12:20:15 +1000

    chore: improving postgres test failure assertions

--- tests/postgres_test_failure-path1.rs
+++ tests/postgres_test_failure-path1.rs
@@ -16,6 +16,15 @@ mod postgres_failure_path_tests {
             .arg("--db-pass=postgres");
         cmd.assert()
             .failure()
+            // src/index.ts
+            .stderr(predicates::str::contains("relation \"indexjs_unknown\" does not exist"))
+            .stderr(predicates::str::contains("INSERT has more expressions than target columns"))
+            // src/import-alias.ts
+            .stderr(predicates::str::contains("relation \"aliased_unknown\" does not exist"))
+            // src/nested/more-nested/more-nested
+            .stderr(predicates::str::contains("relation \"nested_unknown1\" does not exist"))
+            .stderr(predicates::str::contains("relation \"nested_unknown2\" does not exist"))
+            .stderr(predicates::str::contains("relation \"nested_unknown3\" does not exist"))
             .stdout(predicates::str::contains("SQLs failed to compile!"));
 
         Ok(())
