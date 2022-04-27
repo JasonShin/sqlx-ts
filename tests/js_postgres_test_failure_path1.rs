31d43443b004a0e836b8c31005a19c2274ca23b5
Jason Shin <visualbbasic@gmail.com>
2022-04-27 22:24:12 +1000

    chore: adding js_postgres test failure path

--- tests/js_postgres_test_failure_path1.rs
+++ tests/js_postgres_test_failure_path1.rs
@@ -1,1 +1,45 @@
+#[cfg(test)]
+mod js_postgres_failure_path_tests {
+    use assert_cmd::prelude::*;
+    use predicates::prelude::*;
+    use std::process::Command;
 
+    /// should be using all CLI args to provide credential for DB connection
+    #[test]
+    fn failure_with_all_cli_args() -> Result<(), Box<dyn std::error::Error>> {
+        let mut cmd = Command::cargo_bin("sqlx-ts").unwrap();
+
+        cmd.arg("samples/generic/js-failure-path1")
+            .arg("--ext=js")
+            .arg("--db-host=127.0.0.1")
+            .arg("--db-port=54321")
+            .arg("--db-user=postgres")
+            .arg("--db-pass=postgres");
+        cmd.assert()
+            .failure()
+            // src/index.ts
+            .stderr(predicates::str::contains(
+                "relation \"indexjs_unknown\" does not exist",
+            ))
+            .stderr(predicates::str::contains(
+                "INSERT has more expressions than target columns",
+            ))
+            // src/import-alias.ts
+            .stderr(predicates::str::contains(
+                "relation \"aliased_unknown\" does not exist",
+            ))
+            // src/nested/more-nested/more-nested
+            .stderr(predicates::str::contains(
+                "relation \"nested_unknown1\" does not exist",
+            ))
+            .stderr(predicates::str::contains(
+                "relation \"nested_unknown2\" does not exist",
+            ))
+            .stderr(predicates::str::contains(
+                "relation \"nested_unknown3\" does not exist",
+            ))
+            .stdout(predicates::str::contains("SQLs failed to compile!"));
+
+        Ok(())
+    }
+}
