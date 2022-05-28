759f8131d5bd98fa5157b18dba8537321c5f191b
Jason Shin <visualbbasic@gmail.com>
2022-05-28 17:33:45 +1000

    chore: step 1 to take multiple database connections

--- sqlx-ts-common/src/config.rs
+++ sqlx-ts-common/src/config.rs
@@ -1,13 +1,25 @@
+use crate::cli::{Cli, DatabaseType};
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
-use crate::cli::{Cli};
 use std::env::var;
+use std::fs;
+use std::path::{PathBuf};
+use std::str::FromStr;
+use serde;
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct DbConnectionConfig {
+    #[serde(rename="DB_TYPE")]
+    pub db_type: String,
+    #[serde(rename="DB_HOST")]
     pub db_host: String,
+    #[serde(rename="DB_PORT")]
     pub db_port: i32,
+    #[serde(rename="DB_USER")]
     pub db_user: String,
+    #[serde(rename="DB_PASS")]
     pub db_pass: Option<String>,
+    #[serde(rename="DB_NAME")]
     pub db_name: Option<String>,
 }
 
@@ -22,7 +34,7 @@ pub struct Config {
     pub db_user: String,
     pub db_pass: Option<String>,
     pub db_name: Option<String>,
-    pub connections: HashMap<String, DbConnectionConfig>,
+    pub connections: Option<HashMap<String, DbConnectionConfig>>,
 }
 
 fn required_var_msg(key: &str) -> String {
@@ -34,12 +46,11 @@ fn required_var_msg(key: &str) -> String {
 
 impl Config {
     pub fn new(cli_args: Cli) -> Config {
-        let json_config_path = cli_args.config;
-        println!("json config path {:?}", json_config_path);
+        let cli_args = &cli_args;
 
-        return Config {
-            db_host: match cli_args.db_host {
-                Some(db_host) => db_host,
+        Config {
+            db_host: match &cli_args.db_host {
+                Some(db_host) => db_host.to_owned(),
                 None => var("DB_HOST").expect(required_var_msg("DB_HOST").as_str()),
             },
             db_port: match cli_args.db_port {
@@ -53,20 +64,30 @@ impl Config {
                     })
                     .expect(required_var_msg("DB_PORT").as_str()),
             },
-            db_user: match cli_args.db_user {
-                Some(db_user) => db_user,
+            db_user: match &cli_args.db_user {
+                Some(db_user) => db_user.to_owned(),
                 None => var("DB_USER").expect(required_var_msg("DB_USER").as_str()),
             },
-            db_pass: match cli_args.db_pass {
-                Some(db_pass) => Some(db_pass),
+            db_pass: match &cli_args.db_pass {
+                Some(db_pass) => Some(db_pass.to_owned()),
                 None => var("DB_PASS").ok(),
             },
-            db_name: match cli_args.db_name {
-                Some(db_name) => Some(db_name),
+            db_name: match &cli_args.db_name {
+                Some(db_name) => Some(db_name.to_owned()),
                 None => var("DB_NAME").ok(),
             },
-            connections: HashMap::new(),
-        };
+            connections: Self::build_connection_configs(&cli_args),
+        }
+    }
+
+    fn build_connection_configs(cli_args: &Cli) -> Option<HashMap<String, DbConnectionConfig>> {
+        let default_config_path = PathBuf::from_str(".sqlxrc.json").unwrap();
+        let file_config_path = &cli_args.config.clone().unwrap_or(default_config_path);
+        let file_based_config = fs::read_to_string(&file_config_path);
+        if let Ok(file_based_config) = file_based_config {
+            serde_json::from_str(&file_based_config).unwrap()
+        }
+        None
     }
 
     pub fn get_postgres_cred(&self) -> String {
--- sqlx-ts-common/src/lib.rs
+++ sqlx-ts-common/src/lib.rs
@@ -1,3 +1,5 @@
+#[macro_use]
+extern crate serde_derive;
 extern crate core;
 
 use swc_common::MultiSpan;
