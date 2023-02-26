1aeae11bf36d69f029bb931348ab179f7809f72d
Jason Shin <visualbbasic@gmail.com>
2023-02-26 22:58:32 +1100

    test: adding mysql test for insert params using pretty assertions

--- tests/mysql_insert_query_parameters.rs
+++ tests/mysql_insert_query_parameters.rs
@@ -1,1 +1,69 @@
+#[cfg(test)]
+mod mysql_insert_query_parameters {
+    use assert_cmd::prelude::*;
+    use predicates::prelude::*;
+    use std::fs;
+    use std::io::Write;
+    use std::process::Command;
+    use tempfile::tempdir;
+    use pretty_assertions::{assert_eq};
 
+    use test_utils::test_utils::TSString;
+
+    #[test]
+    fn should_pick_query_params_from_single_row_of_values() -> Result<(), Box<dyn std::error::Error>> {
+        // SETUP
+        let dir = tempdir()?;
+        let parent_path = dir.path();
+        let file_path = parent_path.join("index.ts");
+
+        let index_content = r#"
+import { sql } from "sqlx-ts";
+
+const someInputQuery = sql`
+INSERT INTO items (id, food_type, time_takes_to_cook, table_id, points)
+VALUES
+(?, ?, 2, 1, 2);
+`
+        "#;
+        let mut temp_file = fs::File::create(&file_path)?;
+        writeln!(temp_file, "{}", index_content)?;
+
+        // EXECUTE
+        let mut cmd = Command::cargo_bin("sqlx-ts").unwrap();
+
+        cmd.arg(parent_path.to_str().unwrap())
+            .arg("--ext=ts")
+            .arg("--db-type=mysql")
+            .arg("--db-host=127.0.0.1")
+            .arg("--db-port=33306")
+            .arg("--db-user=root")
+            .arg("--db-name=sqlx-ts")
+            .arg("-g");
+
+
+        // ASSERT
+        cmd.assert().success();
+
+        let type_file = fs::read_to_string(parent_path.join("index.queries.ts"))?;
+        let type_file = type_file.trim();
+        let gen_query_types = r#"
+export type SomeInputQueryParams = [[number, string]];
+
+export interface ISomeInputQueryResult {
+    
+};
+
+export interface ISomeInputQueryQuery {
+    params: SomeInputQueryParams;
+    result: ISomeInputQueryResult;
+};
+        "#;
+
+        assert_eq!(
+            gen_query_types.trim().to_string().flatten(),
+            type_file.to_string().flatten()
+        );
+        Ok(())
+    }
+}
