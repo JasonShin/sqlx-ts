31a2bddc1b7673f0720062e2c208c336af328501
Jason Shin <visualbbasic@gmail.com>
2023-05-13 14:37:29 +1000

    chore: adding annotations test

---
+++ tests/demo/annotations/annotations.ts
@@ -0,0 +1,15 @@
+import { sql } from 'sqlx-ts'
+
+const sql1 = sql`
+-- @name: test mysql query
+-- @db: db_mysql
+SELECT *
+FROM items;
+`
+
+const sql2 = sql`
+-- @name: test postgres query
+-- @db: default
+SELECT *
+FROM items;
+`
