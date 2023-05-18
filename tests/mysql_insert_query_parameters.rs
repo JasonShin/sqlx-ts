0adccb7907ffcc68d51f36ed2d24d58b8c9a4ded
Jason Shin <visualbbasic@gmail.com>
2023-05-18 22:59:17 +1000

    chore: refactor tests

--- tests/mysql_insert_query_parameters.rs
+++ tests/mysql_insert_query_parameters.rs
@@ -12,19 +12,6 @@ mod mysql_insert_query_parameters {
     use test_utils::{run_test, sandbox::TestConfig};
 
     #[rustfmt::skip]
-run_test!(should_pick_query_params_from_in_list, TestConfig::new("mysql"),
-
-//// TS query ////
-r#"
-
-"#,
-
-//// Generated TS interfaces ////
-r#"
-
-"#);
-
-#[rustfmt::skip]
 run_test!(should_pick_query_params_from_single_row_of_values, TestConfig::new("mysql"),
 
 //// TS query ////
@@ -50,14 +37,11 @@ export interface ISomeInputQueryQuery {
 };
 "#);
 
-    #[test]
-    fn should_pick_query_params_from_multiple_rows_of_values() -> Result<(), Box<dyn std::error::Error>> {
-        // SETUP
-        let dir = tempdir()?;
-        let parent_path = dir.path();
-        let file_path = parent_path.join("index.ts");
+    #[rustfmt::skip]
+run_test!(should_pick_query_params_from_multiple_rows_of_values, TestConfig::new("mysql"),
 
-        let index_content = r#"
+//// TS query ////
+r#"
 import { sql } from "sqlx-ts";
 
 const someInputQuery = sql`
@@ -66,28 +50,10 @@ VALUES
 (?, ?, 2, 1, 2),
 (1, 'test', ?, ?, ?);
 `
-        "#;
-        let mut temp_file = fs::File::create(file_path)?;
-        writeln!(temp_file, "{}", index_content)?;
+"#,
 
-        // EXECUTE
-        let mut cmd = Command::cargo_bin("sqlx-ts").unwrap();
-
-        cmd.arg(parent_path.to_str().unwrap())
-            .arg("--ext=ts")
-            .arg("--db-type=mysql")
-            .arg("--db-host=127.0.0.1")
-            .arg("--db-port=33306")
-            .arg("--db-user=root")
-            .arg("--db-name=sqlx-ts")
-            .arg("-g");
-
-        // ASSERT
-        cmd.assert().success();
-
-        let type_file = fs::read_to_string(parent_path.join("index.queries.ts"))?;
-        let type_file = type_file.trim();
-        let gen_query_types = r#"
+//// Generated TS interfaces ////
+r#"
 export type SomeInputQueryParams = [[number, string], [number, number, number]];
 
 export interface ISomeInputQueryResult {
@@ -98,12 +64,5 @@ export interface ISomeInputQueryQuery {
     params: SomeInputQueryParams;
     result: ISomeInputQueryResult;
 };
-        "#;
-
-        assert_eq!(
-            gen_query_types.trim().to_string().flatten(),
-            type_file.to_string().flatten()
-        );
-        Ok(())
-    }
+"#);
 }
