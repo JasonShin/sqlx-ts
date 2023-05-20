7f877a47e7432bbc3bd5b8e2451f5d45a987f0b2
Jason Shin <visualbbasic@gmail.com>
2023-05-20 11:45:32 +1000

    test: adding snapshot testing

--- tests/demo_happy_path.rs
+++ tests/demo_happy_path.rs
@@ -1,8 +1,12 @@
 #[cfg(test)]
 mod demo_happy_path_tests {
     use assert_cmd::prelude::*;
-    use std::process::Command;
+    use pretty_assertions::assert_eq;
     use std::env::current_dir;
+    use std::fs;
+    use std::io::Write;
+    use std::process::Command;
+    use walkdir::WalkDir;
 
     #[test]
     fn all_demo_should_pass() -> Result<(), Box<dyn std::error::Error>> {
@@ -13,15 +17,39 @@ mod demo_happy_path_tests {
         // EXECUTE
         let mut cmd = Command::cargo_bin("sqlx-ts").unwrap();
         cmd.arg(demo_path.to_str().unwrap())
-          .arg(format!("--ext=ts"))
-          .arg("--config=.sqlxrc.sample.json")
-          .arg("-g");
+            .arg(format!("--ext=ts"))
+            .arg("--config=.sqlxrc.sample.json")
+            .arg("-g");
 
         // ASSERT
-      cmd.assert()
-        .success()
-        .stdout(predicates::str::contains("No SQL errors detected!"));
+        cmd.assert()
+            .success()
+            .stdout(predicates::str::contains("No SQL errors detected!"));
 
-      Ok(())
+        for entry in WalkDir::new(demo_path) {
+            if entry.is_ok() {
+                let entry = entry.unwrap();
+                let path = entry.path();
+                let parent = entry.path().parent().unwrap();
+                let file_name = path.file_name().unwrap().to_str().unwrap().to_string();
+                let snapshot_path = parent.join("snapshot.ts");
+
+                if path.is_file() && file_name.ends_with(".queries.ts") {
+                    let generated_types = fs::read_to_string(path)?;
+
+                    if !snapshot_path.exists() {
+                        let mut snapshot_file = fs::File::create(&snapshot_path)?;
+                        writeln!(snapshot_file, "{}", generated_types)?;
+                    }
+
+                    assert_eq!(
+                        generated_types.trim().to_string().trim(),
+                        fs::read_to_string(&snapshot_path)?.to_string().trim(),
+                    )
+                }
+            }
+        }
+
+        Ok(())
     }
 }
