1a712ac08587604f3eecf697e11b19372d0cab08
Jason Shin <visualbbasic@gmail.com>
2023-05-20 10:40:32 +1000

    test: using macros everywhere

--- tests/postgres_query_parameters.rs
+++ tests/postgres_query_parameters.rs
@@ -9,17 +9,13 @@ mod postgres_query_paramters_tests {
 
     use pretty_assertions::assert_eq;
     use test_utils::test_utils::TSString;
+    use test_utils::{run_test, sandbox::TestConfig};
 
-    #[test]
-    fn should_pick_query_params_from_flat_list_of_binary_ops() -> Result<(), Box<dyn std::error::Error>> {
-        // SETUP
-        let dir = tempdir()?;
-        let parent_path = dir.path();
-        let file_path = parent_path.join("index.ts");
+    #[rustfmt::skip]
+run_test!(should_pick_query_params_from_flat_list_of_binary_ops, TestConfig::new("postgres"),
 
-        let index_content = r#"
-import { sql } from 'sqlx-ts';
-
+//// TS query ////
+r#"
 const someQuery = sql`
 SELECT *
 FROM items
@@ -27,29 +23,10 @@ WHERE points > $1
 AND points < $2
 OR points = $3
 `;
-    "#;
-        let mut temp_file = fs::File::create(file_path)?;
-        writeln!(temp_file, "{}", index_content)?;
+"#,
 
-        // EXECUTE
-        let mut cmd = Command::cargo_bin("sqlx-ts").unwrap();
-
-        cmd.arg(parent_path.to_str().unwrap())
-            .arg("--ext=ts")
-            .arg("--db-type=postgres")
-            .arg("--db-host=127.0.0.1")
-            .arg("--db-port=54321")
-            .arg("--db-user=postgres")
-            .arg("--db-pass=postgres")
-            .arg("--db-name=postgres")
-            .arg("-g");
-
-        // ASSERT
-        cmd.assert().success();
-
-        let type_file = fs::read_to_string(parent_path.join("index.queries.ts"))?;
-        let type_file = type_file.trim();
-        let gen_query_types = r#"
+//// Generated TS interfaces ////
+r#"
 export type SomeQueryParams = [number, number, number];
 
 export interface ISomeQueryResult {
@@ -64,11 +41,5 @@ export interface ISomeQueryQuery {
     params: SomeQueryParams;
     result: ISomeQueryResult;
 };
-        "#;
-        assert_eq!(
-            gen_query_types.trim().to_string().flatten(),
-            type_file.to_string().flatten()
-        );
-        Ok(())
-    }
+"#);
 }
--- tests/postgres_update_query_parameters.rs
+++ tests/postgres_update_query_parameters.rs
@@ -9,44 +9,21 @@ mod postgres_update_query_parameters {
     use tempfile::tempdir;
 
     use test_utils::test_utils::TSString;
+    use test_utils::{run_test, sandbox::TestConfig};
 
-    #[test]
-    fn should_pick_query_params_from_single_row_of_values() -> Result<(), Box<dyn std::error::Error>> {
-        // SETUP
-        let dir = tempdir()?;
-        let parent_path = dir.path();
-        let file_path = parent_path.join("index.ts");
+    #[rustfmt::skip]
+run_test!(should_pick_query_params_from_single_row_of_values, TestConfig::new("postgres"),
 
-        let index_content = r#"
-import { sql } from "sqlx-ts";
-
+//// TS query ////
+r#"
 const someUpdateQuery = sql`
 UPDATE items
 SET food_type = $1;
 `
-        "#;
-        let mut temp_file = fs::File::create(file_path)?;
-        writeln!(temp_file, "{}", index_content)?;
+"#,
 
-        // EXECUTE
-        let mut cmd = Command::cargo_bin("sqlx-ts").unwrap();
-
-        cmd.arg(parent_path.to_str().unwrap())
-            .arg("--ext=ts")
-            .arg("--db-type=postgres")
-            .arg("--db-host=127.0.0.1")
-            .arg("--db-port=54321")
-            .arg("--db-user=postgres")
-            .arg("--db-pass=postgres")
-            .arg("--db-name=postgres")
-            .arg("-g");
-
-        // ASSERT
-        cmd.assert().success();
-
-        let type_file = fs::read_to_string(parent_path.join("index.queries.ts"))?;
-        let type_file = type_file.trim();
-        let gen_query_types = r#"
+//// Generated TS interfaces ////
+r#"
 export type SomeUpdateQueryParams = [string];
 
 export interface ISomeUpdateQueryResult {
@@ -57,12 +34,5 @@ export interface ISomeUpdateQueryQuery {
     params: SomeUpdateQueryParams;
     result: ISomeUpdateQueryResult;
 };
-        "#;
-
-        assert_eq!(
-            gen_query_types.trim().to_string().flatten(),
-            type_file.to_string().flatten()
-        );
-        Ok(())
-    }
+"#);
 }
