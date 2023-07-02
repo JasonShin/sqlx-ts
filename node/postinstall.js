f392727f6c759290f65675a85bbb9ababa097a29
Jason Shin <visualbbasic@gmail.com>
2023-07-02 22:17:06 +1000

    chore: postinstall using tag

--- node/postinstall.js
+++ node/postinstall.js
@@ -1,8 +1,8 @@
 let execSync = require('child_process').execSync
+let tag = require('./package.json').version
 
 const os = process.platform
 const cpu = process.arch
 
-console.error('testing installation')
-execSync(`curl -LSfs https://jasonshin.github.io/sqlx-ts/install.sh | bash -s -- --os ${os} --cpu ${cpu} -f`, { stdio: 'inherit' })
-console.error('sqlx-ts installation successful')
+execSync(`curl -LSfs https://jasonshin.github.io/sqlx-ts/install.sh | bash -s -- --os ${os} --cpu ${cpu} --tag ${tag} -f`, { stdio: 'inherit' })
+console.info('sqlx-ts installation successful')
