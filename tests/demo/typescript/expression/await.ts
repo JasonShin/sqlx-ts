7a2d8ab029aeffcc1d027b86f014013cfa3203c1
Jason Shin <visualbbasic@gmail.com>
2023-08-20 18:59:52 +1000

    test: adding await examples

--- tests/demo/typescript/expression/await.ts
+++ tests/demo/typescript/expression/await.ts
@@ -7,5 +7,16 @@ SELECT id FROM items;
 `
 }
 
-// lambda
-// ffi
+const asyncLambdaAwaited = async () => await sql`SELECT id FROM items`
+
+const asyncLambda = async () => sql`SELECT id FROM items`
+
+// TODO: iif function is not working
+(async function () {
+    const iifFunction = sql`SELECT id FROM items;`
+})()
+
+(async () => sql`
+-- @name: iif lambda
+SELECT id FROM items;
+`)()
