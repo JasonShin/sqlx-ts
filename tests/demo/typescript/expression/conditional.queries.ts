db656acf02d127a64f8af20c271d1072dc610049
Jason Shin <visualbbasic@gmail.com>
2023-08-26 23:34:50 +1000

    chore: updating conditional queries

---
+++ tests/demo/typescript/expression/conditional.queries.ts
@@ -0,0 +1,31 @@
+
+
+export type TruthyParams = [];
+
+
+export interface ITruthyResult {
+    id: number;
+};
+
+
+export interface ITruthyQuery {
+    params: TruthyParams;
+    result: ITruthyResult;
+};
+
+
+
+
+export type FalsyParams = [];
+
+
+export interface IFalsyResult {
+    id: number;
+};
+
+
+export interface IFalsyQuery {
+    params: FalsyParams;
+    result: IFalsyResult;
+};
+
