d5b67e63ca0c13a55408be0ab6a80b7f95438d6d
Jason Shin <visualbbasic@gmail.com>
2023-09-16 14:45:52 +0900

    chore: adding panic test

--- tests/alias.rs
+++ tests/alias.rs
@@ -42,6 +42,38 @@ JOIN tables ON items.table_id = tables.id
         Ok(())
     }
 
+
+    #[test]
+    #[should_panic(expected = "Impossible to calculate appropriate field names of a wildcard query with multiple tables.")]
+    fn should_not_warn_on_field_names_of_asterix() {
+        let ts_content = r#"
+        const someQuery = sql`
+        SELECT *
+        FROM items
+        `"#;
+
+        // SETUP
+        let dir = tempdir().unwrap();
+        let parent_path = dir.path();
+        let file_path = parent_path.join(format!("index.ts"));
+        let mut temp_file = fs::File::create(&file_path).unwrap();
+        writeln!(temp_file, "{}", ts_content).unwrap();
+
+        // EXECUTE
+        let mut cmd = Command::cargo_bin("sqlx-ts").unwrap();
+        cmd.arg(parent_path.to_str().unwrap())
+            .arg("--ext=ts")
+            .arg("--config=.sqlxrc.sample.json")
+            .arg("-g");
+
+        // ASSERT
+        cmd.assert()
+            .success()
+            // check not condition
+            .try_stdout(predicates::str::contains("Impossible to calculate appropriate field names of a wildcard query with multiple tables."))
+            .unwrap();
+    }
+
     #[test]
     fn should_warn_on_clashing_field_names_on_join_asterix() -> Result<(), Box<dyn std::error::Error>> {
         let ts_content = r#"
@@ -67,9 +99,7 @@ JOIN tables ON items.table_id = tables.id
 
         // ASSERT
         cmd.assert().success()
-               .stdout(predicates::str::contains("Missing an alias for a compound identifier, using items_table_id as the key name. Prefer adding an alias for example: `items.table_id AS table_id`"))
-               .stdout(predicates::str::contains("Missing an alias for a compound identifier, using items_id as the key name. Prefer adding an alias for example: `items.id AS id`"))
-               .stdout(predicates::str::contains("Missing an alias for a compound identifier, using tables_id as the key name. Prefer adding an alias for example: `tables.id AS id`"))
+               .stdout(predicates::str::contains("Impossible to calculate appropriate field names of a wildcard query with multiple tables."))
               .stdout(predicates::str::contains("No SQL errors detected!"));
 
         Ok(())
