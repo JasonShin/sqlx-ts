52a643df70e957f02ff9cc950f714c84ff76b406
Jason Shin <visualbbasic@gmail.com>
2023-10-01 21:39:50 +0900

    chore: remove SOME_INT

--- src/common/lazy.rs
+++ src/common/lazy.rs
@@ -14,8 +14,6 @@ use std::sync::{Arc, Mutex};
 // The file contains all implicitly dependent variables or state that files need for the logic
 // We have a lot of states that we need to drill down into each methods
 lazy_static! {
-    pub static ref SOME_INT: i32 = 5;
-
     pub static ref CLI_ARGS: Cli = Cli::parse();
     pub static ref CONFIG: Config =  Config::new();
 
