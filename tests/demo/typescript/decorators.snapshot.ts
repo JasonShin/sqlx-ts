125cc9b5125a3a43f30d4232f4ca6990f774e4e1
Jason Shin <visualbbasic@gmail.com>
2023-10-27 22:45:03 +1100

    test: enhance test to cover nest js type sourcefiles

--- tests/demo/typescript/decorators.snapshot.ts
+++ tests/demo/typescript/decorators.snapshot.ts
@@ -1,16 +1,240 @@
 
 
-export type DecoQueryParams = [];
+export type PrivDecoConstructorParams = [];
 
 
-export interface IDecoQueryResult {
+export interface IPrivDecoConstructorResult {
     id: number;
 };
 
 
-export interface IDecoQueryQuery {
-    params: DecoQueryParams;
-    result: IDecoQueryResult;
+export interface IPrivDecoConstructorQuery {
+    params: PrivDecoConstructorParams;
+    result: IPrivDecoConstructorResult;
+};
+
+
+
+
+export type PrivDecoMethodParams = [];
+
+
+export interface IPrivDecoMethodResult {
+    id: number;
+};
+
+
+export interface IPrivDecoMethodQuery {
+    params: PrivDecoMethodParams;
+    result: IPrivDecoMethodResult;
+};
+
+
+
+
+export type PrivDecoMethodParams = [];
+
+
+export interface IPrivDecoMethodResult {
+    id: number;
+};
+
+
+export interface IPrivDecoMethodQuery {
+    params: PrivDecoMethodParams;
+    result: IPrivDecoMethodResult;
+};
+
+
+
+
+export type PubDecoMethodParams = [];
+
+
+export interface IPubDecoMethodResult {
+    id: number;
+};
+
+
+export interface IPubDecoMethodQuery {
+    params: PubDecoMethodParams;
+    result: IPubDecoMethodResult;
+};
+
+
+
+
+export type ProtDecoMethodParams = [];
+
+
+export interface IProtDecoMethodResult {
+    id: number;
+};
+
+
+export interface IProtDecoMethodQuery {
+    params: ProtDecoMethodParams;
+    result: IProtDecoMethodResult;
+};
+
+
+
+
+export type PrivDecoConstructorParams = [];
+
+
+export interface IPrivDecoConstructorResult {
+    id: number;
+};
+
+
+export interface IPrivDecoConstructorQuery {
+    params: PrivDecoConstructorParams;
+    result: IPrivDecoConstructorResult;
+};
+
+
+
+
+export type PrivDecoMethodParams = [];
+
+
+export interface IPrivDecoMethodResult {
+    id: number;
+};
+
+
+export interface IPrivDecoMethodQuery {
+    params: PrivDecoMethodParams;
+    result: IPrivDecoMethodResult;
+};
+
+
+
+
+export type PrivDecoMethodParams = [];
+
+
+export interface IPrivDecoMethodResult {
+    id: number;
+};
+
+
+export interface IPrivDecoMethodQuery {
+    params: PrivDecoMethodParams;
+    result: IPrivDecoMethodResult;
+};
+
+
+
+
+export type PubDecoMethodParams = [];
+
+
+export interface IPubDecoMethodResult {
+    id: number;
+};
+
+
+export interface IPubDecoMethodQuery {
+    params: PubDecoMethodParams;
+    result: IPubDecoMethodResult;
+};
+
+
+
+
+export type ProtDecoMethodParams = [];
+
+
+export interface IProtDecoMethodResult {
+    id: number;
+};
+
+
+export interface IProtDecoMethodQuery {
+    params: ProtDecoMethodParams;
+    result: IProtDecoMethodResult;
+};
+
+
+
+
+export type PrivDecoConstructorParams = [];
+
+
+export interface IPrivDecoConstructorResult {
+    id: number;
+};
+
+
+export interface IPrivDecoConstructorQuery {
+    params: PrivDecoConstructorParams;
+    result: IPrivDecoConstructorResult;
+};
+
+
+
+
+export type PrivDecoMethodParams = [];
+
+
+export interface IPrivDecoMethodResult {
+    id: number;
+};
+
+
+export interface IPrivDecoMethodQuery {
+    params: PrivDecoMethodParams;
+    result: IPrivDecoMethodResult;
+};
+
+
+
+
+export type PrivDecoMethodParams = [];
+
+
+export interface IPrivDecoMethodResult {
+    id: number;
+};
+
+
+export interface IPrivDecoMethodQuery {
+    params: PrivDecoMethodParams;
+    result: IPrivDecoMethodResult;
+};
+
+
+
+
+export type PubDecoMethodParams = [];
+
+
+export interface IPubDecoMethodResult {
+    id: number;
+};
+
+
+export interface IPubDecoMethodQuery {
+    params: PubDecoMethodParams;
+    result: IPubDecoMethodResult;
+};
+
+
+
+
+export type ProtDecoMethodParams = [];
+
+
+export interface IProtDecoMethodResult {
+    id: number;
+};
+
+
+export interface IProtDecoMethodQuery {
+    params: ProtDecoMethodParams;
+    result: IProtDecoMethodResult;
 };
 
 
