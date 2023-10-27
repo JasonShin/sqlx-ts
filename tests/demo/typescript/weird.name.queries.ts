331912320c91ad1a60b837de5d1bd9ac69c629e4
Jason Shin <visualbbasic@gmail.com>
2023-10-28 01:06:30 +1100

    weird names

---
+++ tests/demo/typescript/weird.name.queries.ts
@@ -0,0 +1,15 @@
+
+
+export type WeirdNameParams = [];
+
+
+export interface IWeirdNameResult {
+    id: number;
+};
+
+
+export interface IWeirdNameQuery {
+    params: WeirdNameParams;
+    result: IWeirdNameResult;
+};
+
