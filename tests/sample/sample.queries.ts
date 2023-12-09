028d85905b9b7b6ffa02d82c02c19f3cf3fe26a6
Jason Shin <visualbbasic@gmail.com>
2023-12-09 15:12:06 +1100

    Enhance docs (#88)
    * docs: restructring docs
    
    * docs: adding more info on install.sh
    
    * docs: configurations
    
    * docs
    
    * wip
    
    * wip
    
    * docs: add the demo videos

--- tests/sample/sample.queries.ts
+++ tests/sample/sample.queries.ts
@@ -1,15 +1,64 @@
 
 
-export type SampleQueryParams = [];
+export type SampleSelectQueryParams = [number];
 
 
-export interface ISampleQueryResult {
-    someId: number;
+export interface ISampleSelectQueryResult {
+    points: number;
+	someId: number;
 };
 
 
-export interface ISampleQueryQuery {
-    params: SampleQueryParams;
-    result: ISampleQueryResult;
+export interface ISampleSelectQueryQuery {
+    params: SampleSelectQueryParams;
+    result: ISampleSelectQueryResult;
+};
+
+
+
+
+export type SampleInsertQueryParams = [number];
+
+
+export interface ISampleInsertQueryResult {
+    
+};
+
+
+export interface ISampleInsertQueryQuery {
+    params: SampleInsertQueryParams;
+    result: ISampleInsertQueryResult;
+};
+
+
+
+
+export type SampleUpdateQueryParams = [number, number];
+
+
+export interface ISampleUpdateQueryResult {
+    
+};
+
+
+export interface ISampleUpdateQueryQuery {
+    params: SampleUpdateQueryParams;
+    result: ISampleUpdateQueryResult;
+};
+
+
+
+
+export type SampleDeleteQueryParams = [number];
+
+
+export interface ISampleDeleteQueryResult {
+    
+};
+
+
+export interface ISampleDeleteQueryQuery {
+    params: SampleDeleteQueryParams;
+    result: ISampleDeleteQueryResult;
 };
 
--- tests/sample/sample.ts
+++ tests/sample/sample.ts
@@ -1,5 +1,17 @@
 import { sql } from 'sqlx-ts'
 
-const sampleQuery = sql`
-SELECT id as some_id FROM items
+const sampleSelectQuery = sql`
+SELECT id as some_id, points FROM items WHERE id = $1
+`
+
+const sampleInsertQuery = sql`
+INSERT INTO items (points) VALUES ($1)
+`
+
+const sampleUpdateQuery = sql`
+UPDATE items SET points = $1 WHERE id = $2
+`
+
+const sampleDeleteQuery = sql`
+DELETE FROM items WHERE id = $1
 `
