f8b77fdbd6d48749cb4522b6f379e42fa0046092
Jason Shin <visualbbasic@gmail.com>
2024-06-16 17:34:42 +1000

    test: add returning test

---
+++ tests/demo/insert/returning.snapshot.ts
@@ -0,0 +1,52 @@
+
+
+export type WildcardParams = [];
+
+
+export interface IWildcardResult {
+    foodType: string;
+	id: number;
+	points: number;
+	tableId: number;
+	timeTakesToCook: number;
+};
+
+
+export interface IWildcardQuery {
+    params: WildcardParams;
+    result: IWildcardResult;
+};
+
+
+
+
+export type SelectorParams = [];
+
+
+export interface ISelectorResult {
+    id: number;
+};
+
+
+export interface ISelectorQuery {
+    params: SelectorParams;
+    result: ISelectorResult;
+};
+
+
+
+
+export type AliasParams = [];
+
+
+export interface IAliasResult {
+    id1: number;
+};
+
+
+export interface IAliasQuery {
+    params: AliasParams;
+    result: IAliasResult;
+};
+
+
