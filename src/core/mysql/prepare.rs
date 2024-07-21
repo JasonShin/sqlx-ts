17497b4c721dedccaef1062ebf28f1f625864a89
Jason Shin <visualbbasic@gmail.com>
2024-07-21 19:44:11 +1000

    chore: adding failed to retrieve a connection from the pool

--- src/core/mysql/prepare.rs
+++ src/core/mysql/prepare.rs
@@ -27,7 +27,8 @@ pub async fn prepare(
     let explain_query = format!("PREPARE stmt FROM \"{}\"", sql.query);
     let span = sql.span.to_owned();
     let conn = conn.lock().await;
-    let mut conn = conn.get().await.unwrap();
+    let mut conn = conn.get().await
+      .expect("Failed to retrieve a connection from the pool. Consider increasing the connection pool size");
     let result = conn.query::<Row, String>(explain_query).await;
 
     if let Err(err) = result {
