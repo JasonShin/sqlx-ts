c1ad7db5178e2591ecb4189128fa4893601e1177
JasonShin <visualbbasic@gmail.com>
2025-02-23 20:56:47 +1100

    docs: fix baseUrl

--- book/docusaurus.config.ts
+++ book/docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://jasonshin.github.io/sqlx-ts/',
+  url: 'https://jasonshin.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/sqlx-ts/',
