d2422f651b8c208600b31d287e7a0a49b5d3e671
JasonShin <visualbbasic@gmail.com>
2025-08-19 20:09:21 +1000

    test bash

--- node/postinstall.js
+++ node/postinstall.js
@@ -4,5 +4,5 @@ let tag = require('./package.json').version
 const os = process.platform
 const cpu = process.arch
 
-execSync(`curl -LSfs https://jasonshin.github.io/sqlx-ts/install.sh | sh -s -- --os ${os} --cpu ${cpu} --tag ${tag} -f`, { stdio: 'inherit' })
+execSync(`curl -LSfs https://jasonshin.github.io/sqlx-ts/install.sh | bash -s -- --os ${os} --cpu ${cpu} --tag ${tag} -f`, { stdio: 'inherit' })
 console.info('sqlx-ts installation successful')
