2a69236de9a8a656407799886e7308ba8cc55045
JasonShin <visualbbasic@gmail.com>
2025-08-19 20:12:29 +1000

    test

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 
@@ -126,10 +126,16 @@ if [ -z $tag ]; then
     tag=$(curl --silent "https://api.github.com/repos/jasonshin/sqlx-ts/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
     say_err "Tag: latest ($tag)"
 else
-    if [[ $tag != v* ]]; then
-      echo "received tag does not start with v, it will be prefixed with v - tag: $tag"
-      tag="v$tag"
-    fi
+    case $tag in
+        v*)
+            # Tag already starts with v
+            ;;
+        *)
+            # Tag doesn't start with v, add it
+            echo "received tag does not start with v, it will be prefixed with v - tag: $tag"
+            tag="v$tag"
+            ;;
+    esac
 
     say_err "Tag: $tag"
 fi
