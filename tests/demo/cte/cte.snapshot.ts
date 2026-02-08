a49018a93f4bb0b81cc62e85fe6952923954676c
Jason Shin <visualbbasic@gmail.com>
2026-02-08 17:16:52 +1100

    CTE support (#251)
    * cte support
    
    * fmt

---
+++ tests/demo/cte/cte.snapshot.ts
@@ -0,0 +1,48 @@
+export type SimpleCteParams = [];
+
+export interface ISimpleCteResult {
+	id: number;
+	name: string;
+}
+
+export interface ISimpleCteQuery {
+	params: SimpleCteParams;
+	result: ISimpleCteResult;
+}
+
+export type RankWithCteParams = [];
+
+export interface IRankWithCteResult {
+	id: number;
+	name: string;
+	rk: any;
+}
+
+export interface IRankWithCteQuery {
+	params: RankWithCteParams;
+	result: IRankWithCteResult;
+}
+
+export type MultipleCtesParams = [];
+
+export interface IMultipleCtesResult {
+	id: number;
+	name: string;
+}
+
+export interface IMultipleCtesQuery {
+	params: MultipleCtesParams;
+	result: IMultipleCtesResult;
+}
+
+export type CteWithWildcardParams = [];
+
+export interface ICteWithWildcardResult {
+	id: number;
+	name: string;
+}
+
+export interface ICteWithWildcardQuery {
+	params: CteWithWildcardParams;
+	result: ICteWithWildcardResult;
+}
